3c8482726c036191f99211bdde8dfbd49d137acc
Viloll22 <92787590+Viloll22@users.noreply.github.com>
2021-12-22 08:22:47 +0100

    Add files via upload

---
+++ sql_items.sql
@@ -0,0 +1,4 @@
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`, `price`, `weight`, `durability`) VALUES ('coke_pooch', 'Bolsa de cocaina', -1, 0, 1, 0, -1, 100);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`, `price`, `weight`, `durability`) VALUES ('meth_pooch', 'Bolsa de Metanfetaminas', -1, 0, 1, 0, -1, 100);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`, `price`, `weight`, `durability`) VALUES ('opium_pooch', 'Bolsa de Opio', -1, 0, 1, 0, -1, 100);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`, `price`, `weight`, `durability`) VALUES ('weed_pooch', 'Bolsita de Marihuana', 50, 0, 1, 0, 50, 100);
